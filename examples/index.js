cd96464fd31d364f8c40cba4d1e18335ae27a5dd
Alberto Fernandez Reyes <albertofernandezreyes@gmail.com>
2018-10-20 13:07:45 +0300

    Fix linting issues

--- examples/index.js
+++ examples/index.js
@@ -1,49 +1,30 @@
+import Workit from '../dist/Workit'
 
-/*
- *
- * Script to run in the worker
- *
- */
-const workerRunner = (scope, event) => {
-  /* 
-   * We can import scripts but requires full path
-   * importScripts('http://localhost:3030/embeed.js')
-   */
-  let res = 0
-  for(let i = 0; i <= event.data; i++)
-    for(let j = 0; j <= event.data; j++)
-      res += i
+const workit = new Workit()
 
-  scope.postMessage(res)
-}
-
-
-const workit = new Workit(Worker)
-
-workit.addEventListener('message', function(evt){
+workit.addEventListener('message', function (evt) {
   console.log('Message = ' + evt.data)
 })
 
-workit.addEventListener('messageerror', function(error){
-  console.log('MessageError')
+workit.addEventListener('messageerror', function (error) {
+  console.log('MessageError = ' + error)
 })
 
-workit.addEventListener('error', function(){
+workit.addEventListener('error', function () {
   console.log('Error')
 })
 
-workit.install(function(scope, event){
-  /* 
+workit.install(function (scope, event) {
+  /*
    * We can import scripts but requires full path
    * importScripts('http://localhost:3030/embeed.js')
    */
   let res = 0
-  for(let i = 0; i <= event.data; i++)
-    for(let j = 0; j <= event.data; j++)
-      res += i
+  for (let i = 0; i <= event.data; i++) {
+    for (let j = 0; j <= event.data; j++) { res += i }
+  }
 
   scope.postMessage(res)
 })
 
 workit.postMessage(5000)
-
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* globals Worker, Blob */
 
 /*
  *
@@ -5,83 +6,93 @@
  *
  */
 const STATES = {
-  INIT: 'INIT', 
+  INIT: 'INIT',
   RUNNING: 'RUNNING',
   END: 'END'
 }
 
-function Workit (worker) {
+function Workit () {
   this._state = STATES.INIT
 
   this._statesAllowed = Object.keys(STATES)
   this._listenersAllowed = ['message', 'messageerror', 'error']
 
   this._listeners = {}
-  this._listenersAllowed.forEach( name => this._listeners[name] = [] )
+  this._listenersAllowed.forEach(name => {
+    this._listeners[name] = []
+  })
 }
 
-Workit.prototype._isAllowedState = function _isAllowedState(state){
-  return this._statesAllowed.some( stateAllowed  => stateAllowed === state )
+Workit.prototype._isAllowedState = function _isAllowedState (state) {
+  return this._statesAllowed.some(stateAllowed => stateAllowed === state)
 }
 
-Workit.prototype._setState = function _setState(state){
-  if(!this._isAllowedState(state)){
+Workit.prototype._setState = function _setState (state) {
+  if (!this._isAllowedState(state)) {
     throw new Error('State ' + state + ' not allowed.')
   }
 
-  this._state = state  
+  this._state = state
 }
 
-Workit.prototype._isState = function _isState(state){
+Workit.prototype._isState = function _isState (state) {
   return this._state === state
 }
 
-Workit.prototype.addEventListener = function addEventListener(eventName, cb){
-  if(this._listenersAllowed.some( name => name === eventName )) {
+Workit.prototype.addEventListener = function addEventListener (eventName, cb) {
+  if (this._listenersAllowed.some(name => name === eventName)) {
     this._listeners[eventName].push(cb)
   }
 }
 
-Workit.prototype.removeEventListener = function removeEventListener(eventName, cb){
+Workit.prototype.removeEventListener = function removeEventListener (eventName, cb) {
 
 }
 
-Workit.prototype.terminate = function terminate(){
-  if(this._isState(STATES.RUNNING)) {
-    this._worker.terminate()  
+Workit.prototype.terminate = function terminate () {
+  if (this._isState(STATES.RUNNING)) {
+    this._worker.terminate()
   } else {
     throw new Error('Workit is not running.')
   }
-  
 }
 
-Workit.prototype.install = function install(fn){
+Workit.prototype.install = function install (fn) {
   const script = this._createWorkerScript(fn)
-  const scriptBlob = new Blob([script], {type: 'application/javascript'})
+  const scriptBlob = new Blob([script], { type: 'application/javascript' })
   const scriptUrl = URL.createObjectURL(scriptBlob)
 
   this._worker = new Worker(scriptUrl)
 }
 
-Workit.prototype.postMessage = function postMessage(data){
-  // TODO: Remove all listeners
-  // Install all listeners
-  this._listenersAllowed.forEach( eventName => {
-    // Any action that is triggered will move the worker state to END state
-    this._worker.addEventListener( eventName, () => this._setState(STATES.END) )
+Workit.prototype._registerListeners = function _updateListeners () {
+  this._listenersAllowed.forEach(eventName => {
+    this._listeners[eventName].forEach(listener => {
+      // Remove prev listeners
+      this._worker.removeEventListener(eventName, listener)
 
-    this._listeners[eventName].forEach( listener => 
+      // Register new listeners
       this._worker.addEventListener(eventName, listener)
-    )
-  })
+    })
 
+    // Any action that is triggered will move the worker state to END state
+    this._worker.addEventListener(eventName, () => this._setState(STATES.END))
+  })
+}
+
+Workit.prototype.postMessage = function postMessage (data) {
+  // Update listeners
+  this._registerListeners()
+
+  // Set state to RUNNING
   this._setState(STATES.RUNNING)
 
   // Start Worker
   this._worker.postMessage(data)
 }
 
-Workit.prototype._createWorkerScript = function _createWorkerScript(fn) {
+Workit.prototype._createWorkerScript = function _createWorkerScript (fn) {
   return `const f = ${fn}; self.onmessage = (e) => f(self, e)`
 }
 
+export default Workit
