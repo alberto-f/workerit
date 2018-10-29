986558f44a985a5d9de2660dedaa4b79b7b46540
Alberto Fernandez Reyes <albertofernandezreyes@gmail.com>
2018-10-29 23:48:03 +0200

    WIP - Refactor and add tests

--- dist/workerit.js
+++ dist/workerit.js
@@ -11,6 +11,7 @@
    * Workit Class
    *
    */
+
   const STATES = {
     INIT: 'INIT',
     RUNNING: 'RUNNING',
@@ -29,8 +30,17 @@
     });
   }
 
+  Workerit.STATES = STATES;
+
   Workerit.prototype._isAllowedState = function _isAllowedState (state) {
     return this._statesAllowed.some(stateAllowed => stateAllowed === state)
+  };
+
+  /*
+   * State accessors
+   */
+  Workerit.prototype._getState = function _getState () {
+    return this._state
   };
 
   Workerit.prototype._setState = function _setState (state) {
@@ -45,9 +55,13 @@
     return this._state === state
   };
 
+
+  /*
+   * Wrapping Worker methods
+   */
   Workerit.prototype.addEventListener = function addEventListener (eventName, cb) {
-    if (this._listenersAllowed.some(name => name === eventName)) {
-      this._listeners[eventName].push(cb);
+    if (this._isListenerAllowed(eventName)) {
+      this._addListener(eventName, cb);
     }
   };
 
@@ -55,24 +69,53 @@
 
   };
 
+  Workerit.prototype.postMessage = function postMessage (data) {
+    // Update listeners
+    this._registerListeners();
+
+    // Set state to RUNNING
+    this._setState(Workerit.STATES.RUNNING);
+
+    // Start Worker
+    this._worker.postMessage(data);
+  };
+
   Workerit.prototype.terminate = function terminate () {
-    if (this._isState(STATES.RUNNING)) {
+    if (this._isState(Workerit.STATES.RUNNING)) {
       this._worker.terminate();
     } else {
       throw new Error('Workerit is not running.')
     }
   };
 
-  Workerit.prototype.install = function install (fn) {
-    const script = this._createWorkerScript(fn);
-    const scriptBlob = new Blob([script], { type: 'application/javascript' });
-    const scriptUrl = URL.createObjectURL(scriptBlob);
-
-    this._worker = new Worker(scriptUrl);
+  /*
+   * Listeners
+   */
+  Workerit.prototype._getListenersAllowed = function _getListenersAllowed () {
+    return this._listenersAllowed
   };
 
-  Workerit.prototype._registerListeners = function _updateListeners () {
-    this._listenersAllowed.forEach(eventName => {
+  Workerit.prototype._isListenerAllowed = function _isListenerAllowed (type) {
+    return this._getListenersAllowed().some(name => name === type)
+  };
+
+  Workerit.prototype._getListeners = function _getListeners (type) {
+    if (type !== undefined && this._isListenerAllowed(type)) {
+      return this._listeners[type]
+    } else {
+      return this._listeners
+    }
+  };
+
+  Workerit.prototype._addListener = function _addListener (type, listener) {
+    if (this._isListenerAllowed(type)) {
+      this._listeners[eventName].push(listener);
+    }
+  };
+
+
+  Workerit.prototype._registerListeners = function _registerListeners () {
+    this._getListenersAllowed().forEach(eventName => {
       this._listeners[eventName].forEach(listener => {
         // Remove prev listeners
         this._worker.removeEventListener(eventName, listener);
@@ -82,23 +125,27 @@
       });
 
       // Any action that is triggered will move the worker state to END state
-      this._worker.addEventListener(eventName, () => this._setState(STATES.END));
+      this._worker.addEventListener(eventName, () => this._setState(Workerit.STATES.END));
     });
   };
 
-  Workerit.prototype.postMessage = function postMessage (data) {
-    // Update listeners
-    this._registerListeners();
 
-    // Set state to RUNNING
-    this._setState(STATES.RUNNING);
-
-    // Start Worker
-    this._worker.postMessage(data);
-  };
-
+  /*
+   * Worker script
+   */
   Workerit.prototype._createWorkerScript = function _createWorkerScript (fn) {
     return `const f = ${fn}; self.onmessage = (e) => f(self, e)`
+  };
+
+  /*
+   * Allow install a fn in a worker
+   */
+  Workerit.prototype.install = function install (fn) {
+    const script = this._createWorkerScript(fn);
+    const scriptBlob = new Blob([script], { type: 'application/javascript' });
+    const scriptUrl = URL.createObjectURL(scriptBlob);
+
+    this._worker = new Worker(scriptUrl);
   };
 
   return Workerit;
--- specs/index.js
+++ specs/index.js
@@ -5,7 +5,23 @@ const Workerit = require('../dist/workerit.js')
 describe('Workerit - Public functions', function () {
   describe('#Workerit', function () {
     it('constructor', function () {
+      // Function
       expect(Workerit).to.be.a('function')
+      
+      // State
+      const w = new Workerit()
+      expect(w._getState()).to.equal(Workerit.STATES.INIT)
+
+      const listeners = w._getListeners()
+      expect(Object.keys(listeners).length).to.equal(3)
+
+      w._getListenersAllowed().forEach( listenerName => 
+        expect(
+          Object.keys(
+            w._getListeners(listenerName)
+          ).length
+        ).to.equal(0)
+      )
     })
   })
 
@@ -24,12 +40,22 @@ describe('Workerit - Public functions', function () {
   describe('#terminate', function () {
     it('should', function () {
       expect(Workerit.prototype.terminate).to.be.a('function')
+
+      const w = new Workerit()
+      expect(w.terminate.bind(w)).to.throw('Workerit is not running.')
     })
   })
 
   describe('#install', function () {
-    it('should', function () {
+    xit('should', function () {
       expect(Workerit.prototype.install).to.be.a('function')
+
+      const w = new Workerit()
+      w.install(function(){
+        console.log('hello world')
+      })
+
+      expect(w._worker instanceof Worker).to.be(true)
     })
   })
 
